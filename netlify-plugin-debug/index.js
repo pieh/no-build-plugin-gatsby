acc67f8f0d65771e5fe3d87d21efc6ad92cf58a7
pieh <misiek.piechowiak@gmail.com>
2023-05-06 11:42:25 +0200

    debug lambda

--- netlify-plugin-debug/index.js
+++ netlify-plugin-debug/index.js
@@ -1,5 +1,11 @@
+const { copySync } = require("fs-extra");
+
 module.exports = {
   onBuild: async ({ constants }) => {
     console.trace(`[NETLIFY] constants`, { constants });
+  },
+  onPostBuild: async ({ constants }) => {
+    // copy packaged functions to public
+    copySync(constants.FUNCTIONS_DIST, constants.PUBLISH_DIR);
   },
 };
