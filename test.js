8f9ff371937e9a88c8f456e13e023b21c40bfc58
Michal Piechowiak <misiek.piechowiak@gmail.com>
2023-06-07 09:53:05 +0200

    bump

--- test.js
+++ test.js
@@ -1,12 +1,16 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+// const express = require("express");
+// const app = express();
+// const port = 3000;
 
-const lambda = require(`./.cache/page-ssr/lambda`).default;
+// const lambda = require(`./.cache/page-ssr/lambda`).default;
 
-app.use(express.static("public"));
-app.use(lambda);
+// app.use(express.static("public"));
+// app.use(lambda);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+const fs = require(`fs`);
+
+fs.accessSync(__filename, fs.constants.X_OK);
