458c511e67d5a071b05526c85dd2e0494d0047b4
Michal Piechowiak <misiek.piechowiak@gmail.com>
2023-06-29 11:52:52 +0200

    bump

--- test.js
+++ test.js
@@ -17,13 +17,20 @@
 // process.versions.modules = 5;
 // process.versions = { modules: 5 };
 // console.log(process.versions.modules);
-const vm = require(`vm`);
+// const vm = require(`vm`);
 
-const fs = require(`fs-extra`);
-fs.test = 1;
+// const fs = require(`fs-extra`);
+// fs.test = 1;
 
-console.log({ testReg: require(`fs-extra`).test });
+// console.log({ testReg: require(`fs-extra`).test });
 
-console.log({ testReg2: require(`fs-extra#1`).test });
+// console.log({ testReg2: require(`fs-extra#1`).test });
 
-vm.runInNewContext("console.log({ testVm: require(`fs-extra`).test });", {});
+// vm.runInNewContext("console.log({ testVm: require(`fs-extra`).test });", {});
+
+const path = require(`path`);
+
+const absPath = `/Users/misiek/test/no-build-plugin-gatsby/node_modules/cookie/index.js`;
+const functionFile = `/Users/misiek/test/no-build-plugin-gatsby/.netlify/functions-internal/ssr-engine/ssr-engine.js`;
+
+console.log(`./` + path.relative(functionFile, absPath));
